1c5b27f922687fba92ac8203a6e80c1bc2695039
slatinsky <slatinsky.adam@gmail.com>
2022-03-06 18:42:52 +0100

    EV-138 frontend - building new-frontend in docker

--- new-frontend/src/api/rest.ts
+++ new-frontend/src/api/rest.ts
@@ -2,11 +2,18 @@ import axios from "axios";
 import {configLoaded, parties, texts, vote} from "./stores";
 import {get} from "svelte/store";
 
+// TODO: temporary solution, will call real api in the future
+const base = (import.meta.env.VITE_BASE_PATH ?? "");
+console.log("base", base);
+
+export function url(path: string) {
+    return `${base}${path}`;
+}
 
 export async function loadConfig()
 {
     try {
-        let response = await axios.get("/config_files/config.json");
+        let response = await axios.get(url("/config_files/config.json"));
         return response.data;
     } catch (error) {
         console.error("vyskytla sa chyba getConfig(): " + JSON.stringify(error, null, 2) );
@@ -25,7 +32,7 @@ loadConfig().then(data => {
 export async function sendVoteParliament()
 {
     try {
-        let response = await axios.post("/backend/api/vote_generated", get(vote));
+        let response = await axios.post(url("/../backend/api/vote_generated"), get(vote));
         return response.data;
     } catch (error) {
         console.error("vyskytla sa chyba sendVoteParliament: " + JSON.stringify(error, null, 2) );
--- new-frontend/src/api/websocket.ts
+++ new-frontend/src/api/websocket.ts
@@ -10,3 +10,62 @@ export enum ElectionStatus {
 }
 
 export const electionStatus = writable(ElectionStatus.TOKEN_VALID);
+
+
+const socket = io('/', {
+    path: '/backend/ws/socket.io',
+    transports: ['polling']
+});
+
+socket.on('connect', function (event) {
+    console.log('user is connected now');
+    // socket.emit('client_connect_event', {data: 'User connected'});
+});
+
+socket.on('validated_token', function (msg, cb) {
+    console.log("--------- validated_token", msg, cb);
+
+    if (msg.data == "valid") {
+        console.log("+++++++++++++ validated_token", msg, cb);
+        electionStatus.set(ElectionStatus.TOKEN_VALID);
+    } else if (msg.data == "failed") {
+        electionStatus.set(ElectionStatus.TOKEN_NOT_VALID);
+    } else {
+        alert("WS validated_token - unknown message " + msg.data);
+        electionStatus.set(ElectionStatus.TOKEN_NOT_VALID);
+    }
+});
+
+/**
+ * example
+ * 'actual_state', {
+ *    "state": state
+ * }
+ */
+socket.on('actual_state', function (msg, cb) {
+    console.log("--------- [TODO] actual_state ", msg, cb);
+});
+
+/**
+ * example
+ * 'config', {
+ *           "config": config
+ *       }
+ */
+socket.on('config', function (msg, cb) {
+    console.log("--------- [TODO] config ", msg, cb);
+});
+
+
+// 'saving_vote_message', {
+//     "data": message,
+//     "message": "This message came from vote DB service"
+// }
+socket.on('saving_vote_message', function (msg, cb) {
+    console.log("saving_vote_message", msg, cb);
+});
+
+// socket.emit('client_stop_event', {data: 'Stop doing something'});
+
+
+export {socket};
