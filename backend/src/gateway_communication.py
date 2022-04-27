9dca81970887ed6d2067852a6489100e7933fdb1
Marek Ceľuch <celuchmarek@gmail.com>
2022-04-27 16:35:19 +0200

    EV-184 fix vt 4

--- backend/src/gateway_communication.py
+++ backend/src/gateway_communication.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 import requests
 import asyncio
 import json
@@ -5,7 +6,7 @@ import os
 from src.frontend_communication import change_state_and_send_to_frontend
 
 import src.imports as imports
-from src.imports import ElectionStates, get_validated_token, sio
+from src.imports import ElectionStates, get_validated_token, sio, app
 
 import src.utils
 from src.utils import transform_vote_to_print, encrypt_message, prepare_printing_vote, reg_printer, print_ticket_out
@@ -145,3 +146,56 @@ async def send_vote_to_gateway(vote: dict, status_code=200) -> None:
 
     await prepare_printing_vote(printing_data)
     await print_ticket_out()
+
+
+async def send_current_election_state_to_frontend() -> None:
+    """
+    Method for sending election config to client
+
+    Keyword arguments:
+    config -- election config
+
+    """
+
+    await app.sio.emit(
+        'changed_election_state', {
+            "state": imports.election_state
+        }
+    )
+
+
+async def change_state_and_send_to_frontend(new_state: str) -> None:
+    """
+    Method for changing election state and sending it to client
+
+    """
+    # Continue only if state was changed
+    if new_state == imports.election_state:
+        return
+
+    if new_state == ElectionStates.TOKEN_VALID and imports.election_state == ElectionStates.ELECTIONS_NOT_STARTED:
+        raise HTTPException(status_code=400, detail='Election not started (2)')
+
+    if new_state == ElectionStates.TOKEN_NOT_VALID and imports.election_state == ElectionStates.ELECTIONS_NOT_STARTED:
+        raise HTTPException(status_code=400, detail='Election not started (3)')
+
+    # Do not change to waiting_for_scan if already is user casting to vote - fixes bug, if user scans NFC tag before waiting for NFC tag is shown
+    # if new_state == ElectionStates.WAITING_FOR_NFC_TAG and imports.election_state == ElectionStates.TOKEN_VALID:
+    #     return
+
+    # Does the state exist in enum?
+    if new_state not in [ ElectionStates.ELECTIONS_NOT_STARTED,ElectionStates.WAITING_FOR_NFC_TAG,
+                        ElectionStates.TOKEN_VALID, ElectionStates.TOKEN_NOT_VALID, 
+                        ElectionStates.VOTE_SUCCESS, ElectionStates.VOTE_ERROR ]:
+        print('Invalid state - ' + str(new_state))
+        raise HTTPException(status_code=400, detail='Invalid state - ' + str(new_state))
+
+    # Download config from gateway if election just started
+    if new_state == ElectionStates.WAITING_FOR_NFC_TAG and imports.election_state == ElectionStates.ELECTIONS_NOT_STARTED:
+        await receive_config_from_gateway()
+
+    print("Changed state to:", new_state)
+
+    imports.election_state = new_state
+
+    await send_current_election_state_to_frontend()
